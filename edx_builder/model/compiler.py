56df53396c4cb8f0e138a2bc4bb767a20ac688ba
Yomguithereal <guillaumeplique@msn.com>
2013-11-12 15:11:27 +0100

    Modified gitignore and adding empty compiler for builder

---
+++ edx_builder/model/compiler.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------
+# EdxBuilder Course Compiler
+# -------------------------------------------------------------------
+#
+#
+#   Author : Guillaume Plique
+#   Organization : Sciences-Po Medialab
+#   Version : 0.1.0
+
+# Dependencies
+#=============
+from colifrapy import Model
+
+
+# Main Class
+#===========
+class Compiler(Model):
+    pass
