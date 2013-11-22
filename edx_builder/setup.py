f0043f449094039dc30476a0a69e34a0496c782a
Yomguithereal <guillaumeplique@gmail.com>
2013-11-22 14:38:31 +0100

    Completing setup file

--- edx_builder/setup.py
+++ edx_builder/setup.py
@@ -10,4 +10,34 @@
 import py2exe
 from distutils.core import setup
 
-setup(console=['edx_builder.py build course'])
+config = {
+    'name': 'EdxBuilder',
+    'version': '0.1.0',
+    'description': 'Edx import format preprocessor.',
+    'packages': [
+        'edx_builder',
+        'edx_builder.model',
+        'edx_builder.model.tools',
+        'edx_builder.model.templates',
+        'edx_builder.model.mdx'
+    ],
+    'package-data': {
+        'edx_builder': ['edx_builder/config/*']
+    },
+    'include_package_data': True,
+    'install_requires': [
+        'colifrapy',
+        'pyyaml',
+        'argparse',
+        'Markdown',
+        'python-scribd'
+    ],
+    'entry_points': """
+    # -*- Entry points: -*-
+    [console_scripts]
+    edxbuilder=edx_builder.edx_builder:main
+    """,
+    'console': ['edxbuilder']
+}
+
+setup(**config)
