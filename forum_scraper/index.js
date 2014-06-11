11a7bc5455abee55e4e86bfa55c53a55cacbfda7
Yomguithereal <guillaumeplique@gmail.com>
2014-06-11 16:52:20 +0200

    Working robot

--- forum_scraper/index.js
+++ forum_scraper/index.js
@@ -1,11 +1,28 @@
 ;(function($, undefined) {
 
-  var externalUrls;
+  var DOWNLOAD = true,
+      externalUrls = [],
+      posts = [],
+      imageIndex = 0,
+      current;
+
+  function pad(i) {
+    return ('' + i).length > 1 ? '' + i : '0' + i;
+  }
+
+  var today = new Date(),
+      waybackDate = ''+ today.getUTCFullYear() +
+                    (pad(today.getUTCMonth() + 1)) +
+                    pad(today.getUTCDate()) +
+                    '000000';
 
   // Main
   function archive(nb) {
+    current = nb;
     var $forumButton = $('.board-name:contains(S0' + nb + ')');
     externalUrls = [];
+    posts = [];
+    imageIndex = 0;
 
     // Clicking the button
     $forumButton.simulate('click');
@@ -50,8 +67,20 @@
     //1. Opening post
     if (i !== undefined)
       $('.list-item:eq(' + i + ') > a').simulate('click');
+
     artoo.waitFor(hasFinishedLoading, function() {
       var $post = $('.discussion-article:first');
+
+      // Typically scrapeOne material here
+      var data = {
+        id: $post.attr('data-id'),
+        title: $post.find('h1:first').text(),
+        author: $post.find('.username:first').text(),
+        nb_comments: $post.find('.discussion-response').length,
+        nb_votes: +$post.find('.votes-count-number:first').text()
+      };
+
+      posts.push(data);
 
       // Removing Useless parts
       $post.find('.discussion-reply-new').remove();
@@ -60,6 +89,8 @@
       $post.find('.discussion-pin').remove();
       $post.find('.action-follow').remove();
       $post.find('.vote-btn').remove();
+      $post.find('.new-comment').remove();
+      $post.find('.action-endorse').remove();
 
       // Cleaning up some links
       $post.find('.post-context > a').attr('href', '#');
@@ -70,24 +101,73 @@
       // Head
       $dummy.find('head')
             .append('<meta charset="utf-8" />')
+            .append('<link href="../style.css" rel="stylesheet" type="text/css" />')
             .append('<title>' + $post.find('h1').text() + '</title>');
 
-      // Body
-      $dummy.find('body').append($post.clone());
+      $dummy.find('body').append('<div class="container"><div class="discussion-body"><div class="discussion-column"></div></div></div>');
 
-      // Downloading
-      artoo.saveHtml($dummy.get(0).documentElement.innerHTML, {
-        filename: $post.attr('data-id')
+      //2. Dealing with images
+      $post.find('img').each(function() {
+        var name = (imageIndex++) + $(this).attr('src').split('/').slice(-1)[0];
+
+        if (DOWNLOAD)
+          artoo.saveResource($(this).attr('src'), {filename: name});
+
+        $(this).attr('src', name);
       });
 
+      //3. Dealing with anchors
+      $post.find('a').each(function() {
+        var href = $(this).attr('href');
+
+        if (href.charAt(0) !== '/' && href.charAt(0) !== '#' &&
+            !~href.indexOf('javascript:')) {
+          externalUrls.push(href);
+          $(this).attr('href', 'http://web.archive.org/web/' + waybackDate + '/' + href);
+        }
+
+        if(href.charAt(0) === '/')
+          $(this).attr('href', '#');
+      });
+
+      //4. Cloning Body
+      $dummy.find('.discussion-column').append($post.clone());
+
+      //7. Downloading
+      if (DOWNLOAD)
+        artoo.saveHtml($dummy.get(0).documentElement.innerHTML, {
+          filename: data.id
+        });
+
       if (typeof next === 'function')
-        next('break');
+        next();
     });
   }
 
   function finished(err) {
-    console.log('finished')
-    // artoo.savePrettyJson(externalUrls, {filename: 'externalURL.json'});
+    artoo.log.info('Finished archiving forum n°' + current);
+
+    // Creating a summary
+    var $summary = $(document.implementation.createHTMLDocument());
+
+    $summary.find('head')
+            .append('<meta charset="utf-8" />')
+            .append('<title>Summary</title>');
+
+    $summary.find('body')
+            .append('<ul id="post-list"></ul>');
+
+    posts.sort(function(a, b) {
+      return b.nb_votes - a.nb_votes;
+    }).forEach(function(p) {
+      $summary.find('#post-list').append('<li><a href="' + p.id + '.html">' + p.title + '</a> - <em>' + p.author+ '</em> (Votes: ' + p.nb_votes + ', Comments: ' + p.nb_comments + ')</li>');
+    });
+
+    if (DOWNLOAD) {
+      artoo.saveHtml($summary.get(0).documentElement.innerHTML, {filename: 'summary.html'});
+      artoo.savePrettyJson(externalUrls, {filename: 'externalUrls.json'});
+      artoo.savePrettyJson(posts, {filename: 'posts.json'});
+    }
   }
 
   this.archive = archive;
